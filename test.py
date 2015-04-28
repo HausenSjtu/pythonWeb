2daeb0f7aea642c6ecc3b07898daddac99706c45
HausenSjtu <sjtuzhs@sjtu.edu.cn>
2015-04-28 21:00:01 +0800

    优化了取图，增加了显示进度，去掉长图的功能

--- test.py
+++ test.py
@@ -1,10 +1,33 @@
 import re
+import cv2
+##import numpy
+import urllib2
+import urllib
+import cStringIO
+import os
+
 
 ##z='hi,ni\\ hao'
 ##print z.replace('\\','\\\\')
 ##print z
 
-w='absba123'
-a='zhsth_uy_zhshe'
-b = re.findall(r'.*_(?!sa).*_.*?',a)
-print b
+##w='absba123'
+##a='zhsth_uy_zhshe'
+##b = re.findall(r'.*_(?!sa).*_.*?',a)
+##print b
+
+
+i=10
+l=['0','1','2']
+a=10/15.0
+a=round(a,2)
+print a
+
+
+
+
+##im = cv2.imread('5p_14.jpg')
+##h,w = im.shape[0:2]
+##print h
+##print w
+####os.remove('1p_5.jpg')
--- wallpaper.py
+++ wallpaper.py
@@ -2,30 +2,44 @@ import urllib2
 import urllib
 import re
 import os
-from PIL import Image
-import cStringIO
+import cv2
+##from PIL import Image
+##import cStringIO
 
+startPage = 5
+endPage = 5
 
 
 path = os.getcwd()
 
-for j in range (1,2):
+for j in range (startPage,endPage+1):
        web = urllib2.urlopen('https://yande.re/post?page='+str(j)+'&tags=wallpaper')
        content = web.read()
        pics = re.findall(r'https://files.*?jpg',content)
        
-       for i in range (1):#(len(pics)):
+       for i in range (len(pics)):
               pic = pics[i]
-              file = urllib2.urlopen(pic)
-              tmpIm = cStringIO.StringIO(file.read())
-              print tmpIm
-              im = Image.open(tmpIm)
-              print im
+##              file = urllib2.urlopen(pic)
+##              tmpIm = cStringIO.StringIO(file.read())
+##              print tmpIm
+##              im = Image.open(tmpIm)
+##              print im
               if pic.find('sample')==-1:
-                     urllib.urlretrieve(pic, 'F:\\pics\\'+str(j)+'p_'+str(i)+'.jpg')
+                     fileName = path+'\\pics\\'+str(j)+'p_'+str(i)+'.jpg'
+                     urllib.urlretrieve(pic, fileName)
+                     img = cv2.imread(fileName)
+                     h, w = img.shape[0:2]
+                     if h > w:
+                            os.remove(fileName)
+                     pec = ((j-startPage)+min([i,20])/15.0)/(endPage-startPage+1)
+                     pec *=100
+                     pec = round(pec,0)
+                     print 'mission completed\t'+str(pec)+'%'
+                            
+                     
 
 
-print 'complete~'
+print 'all ok~'
 
 ##f = file('wallpaper.html','w')
 ##f.write(content)
